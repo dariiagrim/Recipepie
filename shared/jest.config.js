9418d4d12732623971551ff61ad179f4fdac4ba0
Andrii Moisol <andrii.moisol@gmail.com>
2021-03-04 13:56:57 +0200

    Add shared

---
+++ shared/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/$1",
+		"^~/(.*)$": "<rootDir>/$1",
+	},
+	moduleFileExtensions: [
+		"ts",
+		"js",
+		"json"
+	],
+	transform: {
+		"^.+\\.ts$": "ts-jest",
+		"^.+\\.js$": "babel-jest",
+	},
+	collectCoverage: true,
+}
